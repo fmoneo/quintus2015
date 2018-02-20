49f2ac3a09dd3877bc571e99ce29547a9cc0ec6f
fmoneo <fmoneo@gmail.com>
2018-02-19 21:09:20 -0800

    Added insecureAuth to mysql connection

--- routes/getResults.js
+++ routes/getResults.js
@@ -14,10 +14,11 @@ router.get('/', function(request, response) {
 
 		var connection = mysql.createConnection(
 		    {
-		      host     : process.env.MYSQL_HOST,
-		      user     : process.env.MYSQL_USER,
-		      password : process.env.MYSQL_PWD,
-		      database : process.env.MYSQL_DB
+		      host         : process.env.MYSQL_HOST,
+		      user         : process.env.MYSQL_USER,
+		      password     : process.env.MYSQL_PWD,
+		      database 	   : process.env.MYSQL_DB,
+		      insecureAuth : true
 		    }
 		);
 
